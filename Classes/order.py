8a5bb278e670ddcdb11083e38db0154cb3f746c5
boulanouar mohamed amine <boulanouarmohamedamine@gmail.com>
2024-04-22 11:58:03 +0100

    added order

--- Classes/order.py
+++ Classes/order.py
@@ -3,10 +3,10 @@ from datetime import date
 class Order:
     
     def __init__(self,id_customer:int,id_produit:int, quantity:int, start_delivery_date:date,end_delivery_date:date, delivery_adresse:str, total:float):
+        
         self.id_customer = id_customer
         self.id_produit = id_produit
         self.quantity = quantity
         self.start_delivery_date = start_delivery_date # the start date of the preferred delivery date
         self.end_delivery_date = end_delivery_date  # the end date of the preferred delivery date
-        self.delivery_adresse = delivery_adresse # same as customer.adresse
-        self.total = total  # calculated as the product price * quantity   
+     
---
+++ Classes/panier.py
@@ -0,0 +1,9 @@
+from typing import List
+from order import Order
+
+class Panier:
+    def __init__(self,order_list:List[Order],delivery_adresse:str,total:float):
+        self.order_list = order_list # list of orders
+        self.delivery_adresse = delivery_adresse # same as customer.adresse   
+        self.total = total  # calculated as the product list price * quantity   
+        
