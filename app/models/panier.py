d4a9572926bcada2dc97fa560050cbccea4eff95
boulanouar mohamed amine <boulanouarmohamedamine@gmail.com>
2024-04-22 14:21:33 +0100

    chore: Add weight column to Order model and update related models

--- app/models/panier.py
+++ app/models/panier.py
@@ -4,7 +4,9 @@ class Panier(db.Model):
     __tablename__ = 'paniers'
 
     id = db.Column(db.Integer, primary_key=True)
+    
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     customer = db.relationship('Customer', back_populates='panier')
-    total = db.Column(db.Float, nullable=False)
+    
     orders = db.relationship('Order', backref='panier')
+  
