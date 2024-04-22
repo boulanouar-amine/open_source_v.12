b7267b16b5a3ee903bc653b6715305a422001011
boulanouar mohamed amine <boulanouarmohamedamine@gmail.com>
2024-04-22 11:53:06 +0100

    refactored basic classes

---
+++ Classes/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    
+    def __init__(self, id:int, nom:str, prenom:str, adresse:str, telephone:str):
+        self.id = id
+        self.nom = nom
+        self.prenom = prenom
+        self.adresse = adresse
+        self.telephone = telephone
---
+++ Classes/product.py
@@ -0,0 +1,7 @@
+class Product:
+    
+    def __init__(self, id:int, nom:str, description:str, price:float):
+        self.id = id
+        self.nom = nom
+        self.description = description
+        self.price = price
